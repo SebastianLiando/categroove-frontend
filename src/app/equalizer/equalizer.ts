c34e40c126b090f4c48e61b39587b6fe7366055a
Sebastian Liando <sebastianliando@gmail.com>
2026-02-28 23:51:34 +0800

    Add equalizer animation component (#10)
    - Add animation keyframes in global CSS file
    - Exclude test files from test coverage

---
+++ src/app/equalizer/equalizer.ts
@@ -0,0 +1,33 @@
+import {Component, computed, input} from '@angular/core';
+import {NgStyle} from '@angular/common';
+
+@Component({
+  selector: 'app-equalizer',
+  imports: [
+    NgStyle
+  ],
+  templateUrl: './equalizer.html',
+  styleUrl: './equalizer.css',
+  host: {
+    class: 'w-full flex flex-row items-end gap-x-1'
+  }
+})
+export class Equalizer {
+  barColor = input("teal")
+  barCount = input(10)
+  animationSpeed = input(1)
+
+  bars = computed(() => Array.from({length: this.barCount()}, (_, index) => index))
+  animationDelays = computed(() => Array.from({length: this.barCount()}, (_) => Math.random() * this.animationSpeed()))
+
+  getStyleForBar(index: number): Partial<CSSStyleDeclaration> {
+    const animationName = index % 2 == 0 ? 'shorteq' : 'talleq'
+    const animationDelay = this.animationDelays()[index]
+
+    return {
+      background: this.barColor(),
+      animationName: animationName,
+      animationDelay: `${animationDelay}s`
+    }
+  }
+}
