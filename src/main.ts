e4a59a5f2c45d5601052b9795ee37f3984bd3021
Sebastian Liando <sebastianliando@gmail.com>
2026-03-22 00:52:22 +0800

    Restore standard app bootstrap via app-root (#22)
    - Restore the use of `<app-root>` component as root component
    - Remove placeholder HTML code in `app.html` and adjust test case accordingly
    - Add login page to route
    - Use top-level await in `main.ts`

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import {LoginPage} from './app/pages/login-page/login-page';
+import { App } from './app/app';
 
-bootstrapApplication(LoginPage, appConfig)
-  .catch((err) => console.error(err));
+try {
+  await bootstrapApplication(App, appConfig)
+} catch(err) {
+  console.error(err)
+}
